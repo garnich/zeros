015e9a630d0ed994fa28f8dc2197462caa5e08ba
garnich <garnich@tut.by>
2018-03-01 15:43:05 +0300

    factorial task

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ module.exports = function getZerosCount(number) {
   var t,z,h;
   var p=0, k=0;
   var res = [];
-  var re = /0+$/g;
   for (i = 1; i<=number; i++){
       t=i;
       while(t%5==0){
         p++;
-        t = Math.trunc(t/5);}}
+        t = Math.floor(t/5);}}
 
   return p;
 
